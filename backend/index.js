ff7abf6d921bb37502a5d8cbf376b04ce3cd98e0
Vivek Yadav <viveky.ec.22@nitj.ac.ingit config --global user.email viveky.ec.22@nitj.ac.in>
2025-03-07 20:34:37 +0530

    run script changed

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ["GET", "POST"],
     credentials: true,
   },
@@ -30,7 +30,7 @@ const io = new Server(server, {
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ["GET", "POST"],
     credentials: true,
   })
